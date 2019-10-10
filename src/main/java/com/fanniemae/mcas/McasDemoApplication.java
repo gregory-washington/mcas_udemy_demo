d1709a048255e5ae7770009239a0c28b0694f548
gregory-washington <gregory_washington@fanniemae.com>
2019-10-10 17:13:50 -0400

    untracked fix

---
+++ src/main/java/com/fanniemae/mcas/McasDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fanniemae.mcas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McasDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McasDemoApplication.class, args);
+	}
+
+}
